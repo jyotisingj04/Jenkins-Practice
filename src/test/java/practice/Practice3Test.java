0a9cd8bb8bea367321be0fb9dc969584fd89cf7d
jyoti <jyoti@Jyoti>
2022-09-23 13:17:48 +0530

    first commit

---
+++ src/test/java/practice/Practice3Test.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice3Test {
+	@Test
+	public void practice3() {
+		Reporter.log("Practice 3", true);
+	}
+}
